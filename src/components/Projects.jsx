a22fb5e4ad0409ab85b46479711ff7f4d31121bf
Vitaliy Garasymovych <vgarasym@depaul.edu>
2024-02-26 21:42:21 -0600

    Changed Projects and ProjectItem components

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,7 +8,7 @@ const Projects = () => {
   return (
     <div id='projects' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
         <h1 className='text-4xl font-bold text-center text-[#001b5e]'>Projects</h1>
-        <p className='text-center py-8'>Here are the projects that I have worked on.</p>
+        <p className='text-center py-8'>Here are the projects that I have worked on. All of the projects should be listed on my GitHub.</p>
         <div className='grid sm:grid-cols-2 gap-12'>
             <ProjectItem img={teslaImg} title='Tesla Website' />
             <ProjectItem img={atlantisImg} title='Atlantis Website' />
