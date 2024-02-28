72429104c370df2e6f3668191ed33ab888a6f793
Vitaliy Garasymovych <vgarasym@depaul.edu>
2024-02-28 17:31:56 -0600

    Changed where resume opens

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -27,7 +27,7 @@ const Sidenav = () => {
                         <AiOutlineProject size={20} />
                         <span className='pl-4'>Projects</span>
                     </a>
-                    <a onClick={handleNav} href="https://www.dropbox.com/scl/fi/ucypzlw24lvpcukmko0xl/Vitaliy-s-Resume-2023.pdf?rlkey=8mprlrl3txxajdf98a0h66sod&dl=0" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                    <a onClick={handleNav} href="https://www.dropbox.com/scl/fi/ucypzlw24lvpcukmko0xl/Vitaliy-s-Resume-2023.pdf?rlkey=8mprlrl3txxajdf98a0h66sod&dl=0" target='_blank' className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
                         <BsPerson size={20} />
                         <span className='pl-4'>Resume</span>
                     </a>
@@ -52,7 +52,7 @@ const Sidenav = () => {
                 <a href="#projects" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
                     <AiOutlineProject size={20}/>
                 </a>
-                <a href="https://www.dropbox.com/scl/fi/ucypzlw24lvpcukmko0xl/Vitaliy-s-Resume-2023.pdf?rlkey=8mprlrl3txxajdf98a0h66sod&dl=0" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+                <a href="https://www.dropbox.com/scl/fi/ucypzlw24lvpcukmko0xl/Vitaliy-s-Resume-2023.pdf?rlkey=8mprlrl3txxajdf98a0h66sod&dl=0" target='_blank' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
                     <BsPerson size={20}/>
                 </a>
                 <a href="#contact" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
