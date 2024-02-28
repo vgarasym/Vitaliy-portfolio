991d987e1be2849916f1c833298644e89a53ec08
Vitaliy Garasymovych <vgarasym@depaul.edu>
2024-02-28 17:28:28 -0600

    Added link to resume in sidenav

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -27,7 +27,7 @@ const Sidenav = () => {
                         <AiOutlineProject size={20} />
                         <span className='pl-4'>Projects</span>
                     </a>
-                    <a onClick={handleNav} href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                    <a onClick={handleNav} href="https://www.dropbox.com/scl/fi/ucypzlw24lvpcukmko0xl/Vitaliy-s-Resume-2023.pdf?rlkey=8mprlrl3txxajdf98a0h66sod&dl=0" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
                         <BsPerson size={20} />
                         <span className='pl-4'>Resume</span>
                     </a>
